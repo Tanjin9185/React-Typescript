b23ae8c04a13b52a943379e8ad881d9e131b9ba7
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-08-17 19:42:40 +0600

    contact

---
+++ src/Components/Contact.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface IProps{
+    name: string,
+    email?: string,
+}
+
+const Contact = ({name, email="N/A"}: IProps) => {
+    return (
+        <div className="card">
+            <p>
+                <strong>Name</strong> {name}
+            </p>
+            <p>
+                <strong>Email</strong> {email}
+            </p>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/Components/Contacts.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Contact from './Contact';
+
+const Contacts = () => {
+
+    const [contact, setContact] = useState('');
+    const [contactList, setContactList] = useState<string[]>([]);
+
+    console.log('contact', contact); 
+
+    const onClick = () => {
+        setContactList([...contactList, contact]);
+        setContact('');
+        // e.preventDefault();
+    }
+    console.log({contactList});
+    return (
+        <div>-
+            <h1>Contact Lists</h1>
+            <div className='from'>
+                <input onChange={(e) => setContact(e.target.value)} name='name' placeholder='name' type="text" />
+                <button onClick={onClick}>Add </button>
+            </div>
+            {
+                contactList.map((contact) => <Contact key={contact} name={contact} email="tanjin.ahamed123@gmail.com"></Contact>)
+            }
+            
+            <Contact name="Maria" email="maria.israt111@gmail.com"></Contact>
+            <Contact name="Tuhin" email="tuhin.ahamed9185@gmail.com"></Contact>
+            <Contact name="Monisa" ></Contact>
+        </div>
+    );
+};
+
+export default Contacts;
